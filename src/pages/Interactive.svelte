6d25c5bb0b1053d11dc2cc3dcff69a7b2606639a
CoopTRUE <coopdog4545@gmail.com>
2024-06-10 23:37:17 -0700

    added svelte shadcn

---
+++ src/pages/Interactive.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import { Button } from '$lib/components/ui/button'
+  let clicked = 0
+</script>
+
+<Button on:click={() => clicked++}>
+  Clicked {clicked}
+  {clicked === 1 ? 'time' : 'times'}
+</Button>
