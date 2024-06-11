bc94b5668ef1b7466bd9f44159429fced434ff48
CoopTRUE <coopdog4545@gmail.com>
2024-06-10 22:58:52 -0700

    import sorting

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,21 @@
 // @ts-check
 
 import eslint from '@eslint/js'
-import tseslint from 'typescript-eslint'
 import eslintPluginAstro from 'eslint-plugin-astro'
+import simpleImportSort from 'eslint-plugin-simple-import-sort'
+import tseslint from 'typescript-eslint'
 
 export default tseslint.config(
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
-  ...eslintPluginAstro.configs.recommended
+  ...eslintPluginAstro.configs.recommended,
+  {
+    plugins: {
+      'simple-import-sort': simpleImportSort,
+    },
+    rules: {
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
+    },
+  }
 )
